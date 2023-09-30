4aa98049bcec54512d9886abe84143cff184c8c0
Benjamin Walkenhorst <krylon@posteo.de>
2023-09-30 18:12:41 +0200

    Add get_logger (copied from an older project)

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: <2023-09-28 20:54:54 krylon>
+# Time-stamp: <2023-09-29 10:05:23 krylon>
 #
 # /data/code/python/memex/common.py
 # created on 28. 09. 2023
@@ -36,9 +36,11 @@ This module contains definitions commonly used throughout the application.
 """
 
 import logging
+import logging.handlers
 import os
 import os.path
 
+from threading import Lock
 from typing import Final
 
 # Base path for application-specific files
@@ -54,15 +56,47 @@ APP_NAME: Final[str] = "Memex"
 APP_VERSION: Final[str] = "0.0.1"
 DEBUG: Final[bool] = True
 
+_lock: Final[Lock] = Lock()
+_cache: Final[dict] = {}
+
 
 def init_app():
     """Initialize the application environment"""
     if not os.path.isdir(BASE_DIR):
         os.mkdir(BASE_DIR)
 
-def get_logger(name):
+
+def get_logger(name: str, terminal: bool = True) -> logging.Logger:
     """Create and return a logger with the given name"""
-    pass
+    with _lock:
+        if name in _cache:
+            return _cache[name]
+
+        log_format = "%(asctime)s (%(name)-16s / line %(lineno)-4d) " + \
+            "- %(levelname)-8s %(message)s"
+        max_log_size = 256 * 2**20
+        max_log_count = 4
+
+        log_obj = logging.getLogger(name)
+        log_obj.setLevel(logging.DEBUG)
+        log_file_handler = logging.handlers.RotatingFileHandler(LOG_PATH,
+                                                                'a',
+                                                                max_log_size,
+                                                                max_log_count)
+
+        log_fmt = logging.Formatter(log_format)
+        log_file_handler.setFormatter(log_fmt)
+        log_obj.addHandler(log_file_handler)
+
+        if terminal:
+            log_console_handler = logging.StreamHandler()
+            log_console_handler.setFormatter(log_fmt)
+            log_console_handler.setLevel(logging.DEBUG)
+            log_obj.addHandler(log_console_handler)
+
+        _cache[name] = log_obj
+        return log_obj
+
 
 # Local Variables: #
 # python-indent: 4 #
